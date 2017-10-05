05e0b14a6a98c1cab2b1abcf6e5263146b5a74c0
Abid Cader <abid.cader@yahoo.fr>
2017-10-05 16:29:37 +0400

    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## I built the functions in following step by step 
+## the given exemples
+## Note:solve(A) Inverse of A where A is a square matrix.
 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) { 	#makeVector <- function(x = numeric()) {
+	s<- NULL						#	m <- NULL
+	set <- function(y) {				#	set <- function(y) {
+	x <<- y						#	x <<- y
+	s <<- NULL						#	m <<- NULL
+	}							#	}
+	get <- function() x				#	get <- function() x
+	setsolve <- function(solve) s <<- solve	#	setmean <- function(mean) m <<- mean
+	getsolve <- function() s			#	getmean <- function() m
+	list(set = set, get = get,			#	list(set = set, get = get,
+	setsolve = setsolve,				#	setmean = setmean,
+	getsolve = getsolve)				#	getmean = getmean)
+	list(set = set, get = get,			#	list(set = set, get = get,
+	setsolve = setsolve,				#	setmean = setmean,
+	getsolve = getsolve)				#	getmean = getmean)
+}								#}
+								
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+
+
+## I write the function in following the model
+
+cacheSolve <- function(x, ...) {			#cachemean <- function(x, ...) {	
+	s <- x$getsolve()					#	m <- x$getmean()
+	if(!is.null(s)) {					#	if(!is.null(m)) {
+		message("getting cached data")	#		message("getting cached data")
+		return(s)					#		return(m)
+	}							#	}
+	data <- x$get()					#	data <- x$get()
+	s <- solve(data, ...)				#	m <- mean(data, ...)
+	x$setsolve(s)					#	x$setmean(m)
+	s							#	m
+}								#}
+								#
+									
+									
+
